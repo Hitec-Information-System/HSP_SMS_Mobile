3071711e47ad961bafca1c513f65aeb43b29c3e7
ymkwon20 <ymkwon@hitecis.co.kr>
2021-10-18 17:51:53 +0900

    [frontend] edit dependencies (delete notification-related package)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,26 +1,10 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 import 'package:frontend/core/presentation/app_widget.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-Future<void> main() async {
-  AwesomeNotifications().initialize(
-    "resource://drawable/res_notification_app_icon",
-    [
-      NotificationChannel(
-        channelKey: "scheduled_channel",
-        channelName: "Scheduled Notifications",
-        defaultColor: const Color(0xFF8983F7),
-        locked: true, // means notifications cannot be dismissible on Android
-        importance: NotificationImportance.High,
-        channelShowBadge: true,
-      )
-    ],
-  );
-  runApp(
-    const ProviderScope(
-      child: AppWidget(),
-    ),
-  );
-}
+void main() => runApp(
+      const ProviderScope(
+        child: AppWidget(),
+      ),
+    );
