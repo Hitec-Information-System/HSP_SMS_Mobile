545dd0e25b3a71c298001d27f310f47091bbd389
ymkwon20 <ymkwon@hitecis.co.kr>
2021-10-20 18:09:21 +0900

    [frontend] block os-triggered popup on app

--- frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
+++ frontend/android/app/src/main/kotlin/com/example/frontend/MainActivity.kt
@@ -1,6 +1,28 @@
 package com.example.frontend
 
+import android.content.Intent
+import android.app.PendingIntent
+import android.nfc.NfcAdapter
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
+    
+    override fun onResume() {
+        super.onResume()
+        val nfcAdapter: NfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        val intent = Intent(this, MainActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        val pendingIntent: PendingIntent = PendingIntent.getActivities(this, 0, arrayOf<Intent>(intent), 0)
+        if (nfcAdapter == null) return
+        val techList = arrayOf<Array<String>>()
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null)
+    }
+
+    
+    override fun onPause() {
+        super.onPause()
+        val nfcAdapter: NfcAdapter = NfcAdapter.getDefaultAdapter(this) ?: return
+        nfcAdapter.disableForegroundDispatch(this)
+    }
+
 }
